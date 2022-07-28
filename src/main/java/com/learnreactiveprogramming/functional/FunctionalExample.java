2568630d6ee1c73ba6b4ac8ca2982db7cb09bc07
n.v.kornilov <n.v.kornilov@tinkoff.ru>
2022-07-28 13:45:33 +0300

    feat: Add lesson-5 code

--- src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        var names = List.of("alex", "ben", "chloe", "adam", "adam");
+        var newNames = namesGreaterThanSize(names, 3);
+        System.out.println(newNames);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> names, int size) {
+        return names.parallelStream()
+            .filter(name -> name.length() > size)
+            .distinct()
+            .map(String::toUpperCase)
+            .sorted()
+            .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,24 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImperativeExample {
+    public static void main(String[] args) {
+        var names = List.of("alex", "ben", "chloe", "adam", "adam");
+        var newNames = namesGreaterThanSize(names, 3);
+        System.out.println(newNames);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> names, int size) {
+        var result = new ArrayList<String>();
+
+        for (String name : names) {
+            if (name.length() > size && !result.contains(name.toUpperCase())) {
+                result.add(name.toUpperCase());
+            }
+        }
+
+        return result;
+    }
 }
