fbb474249c65944ad2985e811ac413edbfb6e8b3
n.v.kornilov <n.v.kornilov@tinkoff.ru>
2022-07-28 14:59:04 +0300

    feat: Add lesson-6 code

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
@@ -2,6 +2,9 @@ package com.learnreactiveprogramming.service;
 
 import java.util.List;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 public class FluxAndMonoSchedulersService {
@@ -12,6 +15,26 @@ public class FluxAndMonoSchedulersService {
     private String upperCase(String name) {
         delay(1000);
         return name.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoSchedulersService service = new FluxAndMonoSchedulersService();
+
+        service.namesFlux()
+            .log()
+            .subscribe(System.out::println);
+
+        service.nameMono()
+            .log()
+            .subscribe(System.out::println);
+    }
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(namesList);
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("alex");
     }
 
 }
